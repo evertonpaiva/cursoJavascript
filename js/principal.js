b97dad049172d5cbf0d778f4cb7417e9b668fddd
Everton Paiva <evertonpaiva@gmail.com>
2019-05-28 12:10:01 -0300

    Calculando o IMC do primeiro paciente.

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,13 @@
-var titulo = document.querySelector(".titulo");
-console.log(titulo);
-console.log(titulo.textContent);
-titulo.textContent = "Alterado";
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / ( altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+console.log(imc);
