bdc3625460cd3955ccdd34d2786f75d5cd573dea
Everton Paiva <evertonpaiva@gmail.com>
2019-05-29 11:26:48 -0300

    Adicionando as validações de campo em branco para as demais propriedades do paciente.

--- js/form.js
+++ js/form.js
@@ -64,21 +64,32 @@ function montaTd(dado, classe){
 }
 
 //Faz a validacao nos dados do paciente inserido
-function validaPaciente(paciente){
+function validaPaciente(paciente) {
+
     var erros = [];
 
-    if( paciente.nome.length == 0){
+    if (paciente.nome.length == 0) {
         erros.push("O nome não pode ser em branco");
     }
 
-    if(!validaPeso(paciente.peso))
-        erros.push("Peso é inválido");
-
-    if(!validaAltura(paciente.altura))
-        erros.push("Altura é inválida");
-
-    if( paciente.gordura.length == 0) {
+    if (paciente.gordura.length == 0) {
         erros.push("A gordura não pode ser em branco");
+    }
+
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if (paciente.altura.length == 0) {
+        erros.push("A altura não pode ser em branco");
+    }
+
+    if (!validaPeso(paciente.peso)) {
+        erros.push("Peso é inválido");
+    }
+
+    if (!validaAltura(paciente.altura)) {
+        erros.push("Altura é inválida");
     }
 
     return erros;
